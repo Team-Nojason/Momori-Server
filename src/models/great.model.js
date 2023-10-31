a951869074c2c3c335667ade973ba1cbedec1f1f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-10-31 20:06:00 +0900

    Add :: Great Repository

--- src/models/great.model.js
+++ src/models/great.model.js
@@ -13,17 +13,25 @@ class GreatModel extends BaseModel {
     findByPostId = async (postId) => {
         const sql = `SELECT * FROM ${this.tableName} WHERE post_id = ?`;
         return (await DBService.query(sql, [postId])).length;
-    }
+    };
+
+    existUserByPostId = async (userId, postId) => {
+        const sql = `SELECT * FROM ${this.tableName} WHERE post_id = ? AND user_id = ?`;
+        const result = await DBService.query(sql, [userId, postId]);
+        return result.length !== 0;
+    };
 
     insert = async (userId, postId) => {
         const sql = `INSERT INTO ${this.tableName}
                     (user_id, postId, created_at)
                     VALUES (?, ?, ?)`;
         await DBService.query(sql, [userId, postId, getCurrentTime()]);
-    }
+    };
 
     delete = async (userId, postId) => {
         const sql = `DELETE FROM ${this.tableName} WHERE user_id = ? AND post_id = ?`;
         await DBService.query(sql, [userId, postId]);
-    }
+    };
 }
+
+module.exports = new GreatModel();
--- src/repositories/great.repository.js
+++ src/repositories/great.repository.js
@@ -1,3 +1,43 @@
+import {getPayloadFromHeader} from "../utils/jwt.util";
+import UserModel from '../models/user.model';
+import GreatModel from '../models/great.model';
+import {ApiExceptions} from "../utils/exceptions/api.exceptions";
+
 class GreatRepository {
-    
+    addGreat = async (header, post_id) => {
+        const payload = await getPayloadFromHeader(header);
+        const {email, platform_type} = payload;
+        const user = await UserModel.findByEmailAndPlatformType(email, platform_type);
+
+        const {user_id} = user;
+
+        if (await GreatModel.existUserByPostId(user_id, post_id)) {
+            throw new ApiExceptions('exist great');
+        }
+        await GreatModel.insert(user_id, post_id);
+    };
+
+    getGreatByPost = async (post_id) => {
+        const posts = await GreatModel.findByPostId(post_id);
+        const len = posts.length;
+        return {
+            post_id: post_id,
+            count: len,
+        }
+    }
+
+    removeGreat = async (header, post_id) => {
+        const payload = await getPayloadFromHeader(header);
+        const {email, platform_type} = payload;
+        const user = await UserModel.findByEmailAndPlatformType(email, platform_type);
+
+        const {user_id} = user;
+
+        await GreatModel.delete(user_id, post_id);
+        const len = (await GreatModel.findByPostId(post_id)).length;
+        return {
+            post_id: post_id,
+            count: len,
+        }
+    }
 }
